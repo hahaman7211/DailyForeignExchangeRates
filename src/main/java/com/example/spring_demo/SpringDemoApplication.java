54fbcda2237fd5b1e5ff793c71d288873f89d361
hahaman7211 <hahamanoo2016@gmail.com>
2024-09-22 23:22:40 +0800

    pom

--- src/main/java/com/example/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/example/spring_demo/SpringDemoApplication.java
@@ -1,38 +1,33 @@
 package com.example.spring_demo;
 
-import com.example.spring_demo.currency.CurrencyRepository;
-import com.example.spring_demo.dailyExchange.DailyExchangeRateService;
-import com.example.spring_demo.dailyExchange.DailyExchangeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
-public class SpringDemoApplication implements CommandLineRunner {
+public class SpringDemoApplication {
 
-	@Autowired
-	private DailyExchangeRateService dailyExchangeRateService;
-
-	@Autowired
-	private DailyExchangeRepository dailyExchangeRepository;
-
-	@Autowired
-	private CurrencyRepository currencyRepository;
+//	@Autowired
+//	private DailyExchangeRateService dailyExchangeRateService;
+//
+//	@Autowired
+//	private DailyExchangeRepository dailyExchangeRepository;
+//
+//	@Autowired
+//	private CurrencyRepository currencyRepository;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-//		dailyExchangeRepository.deleteAll();
-//		dailyExchangeRateService.fetchAndStoreCurrencyData();
-
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+////		dailyExchangeRepository.deleteAll();
+////		dailyExchangeRateService.fetchAndStoreCurrencyData();
+//
+//	}
 
 }
